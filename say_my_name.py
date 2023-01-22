90803ca1d65124d99ff23d3bb014bddd05c8152b
Mirfat Al-Ghaithy <mirfatmohamed@icloud.com>
2023-01-22 22:15:55 +0000

    added file to file to repo

---
+++ say_my_name.py
@@ -0,0 +1,7 @@
+# creating variables that include our first name and last name
+first_name= 'Mirfat'
+last_name= 'Al-Ghaithy'
+
+# print the name function
+print(first_name +' '+last_name)
+# adding space between both names using
